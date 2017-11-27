b177b79c1bf5994fa770e77141128efe9307ed49
Sharmina Akhtar <sakht318@gmail.com>
2017-11-27 16:50:18 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,10 +2,10 @@ class CashRegister
   attr_accessor :total, :discount, :last_transaction
 
 
-  def initialize
+  def initialize (discount = 0)
+    @total = total
     @total = 0
     @discount = discount
-    discount = 0
     @items = []
     @last_transaction = last_transaction
   end
