cb5756115956d819c4c8e57eed36c80eee379ca3
Sharmina Akhtar <sakht318@gmail.com>
2017-11-27 16:58:37 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -34,9 +34,7 @@ class CashRegister
     @items
   end
 
-  def void_last_transaction
-    self.total = 1.76
-    self.last_transaction = 1.76
+  def void_last_transaction(total, last_transaction)
     self.total - self.last_transaction
   end
 end
